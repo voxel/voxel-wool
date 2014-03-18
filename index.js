51d7fd6e73a93dbcd06e8f1a8eafe6ec8699db90
deathcap <deathcap@gmx.co.uk>
2014-03-17 20:04:56 -0700

    Dynamic display name property, too

--- index.js
+++ index.js
@@ -16,23 +16,15 @@ function WoolPlugin(game, opts) {
 }
 
 WoolPlugin.prototype.enable = function() {
-  /*
-  for (var i = 0; i < this.colors.length; i += 1) {
-    var color = this.colors[i];
-
-    // TODO: switch to registerBlocks()
-    this.registry.registerBlock('wool' + ucfirst(color), {texture: 'wool_colored_'+color, displayName: ucfirst(color) + ' Wool'});
-  }
-  */
-
   var self = this;
 
   this.registry.registerBlocks('wool', this.colors.length, {
     texture: function(offset) { 
-               console.log('wool',offset);
                return 'wool_colored_' + (self.colors[offset] || self.colors[0]); 
              },
-    //TODO displayName: function(offset) { return ucfirst(self.colors[offset]) + ' Wool'}
+    displayName: function(offset) { 
+                   return ucfirst(self.colors[offset]) + ' Wool'
+                 },
   });
 };
 
